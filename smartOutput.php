58d6b70061a7742bc7bf7d74046efa92c7536909
Haseeb ur Rehman <professorhaseeb@users.noreply.github.com>
2016-04-20 17:26:47 +0500

    Update smartOutput.php

--- smartOutput.php
+++ smartOutput.php
@@ -1,3 +1,19 @@
 <?php
-  echo "travis check";
+
+class smartOutput{
+  
+  var $output
+  
+  private $name, $value;
+  
+  function __construct(){
+    $this->output = $output;
+  }
+  
+  public function setData($name,$value){
+    
+  }
+  
+}
+
 ?>
