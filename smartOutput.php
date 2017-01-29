8d21f0b8d5a60f8e03525937a401190008e2e619
Haseeb ur Rehman <professorhaseeb@users.noreply.github.com>
2017-01-29 23:19:38 +0500

    Update smartOutput.php

--- smartOutput.php
+++ smartOutput.php
@@ -1,34 +1,57 @@
 <?php
-
 class smartOutput{
-  
   public $output = array();
-  
+  protected $responseOutput = array();
   public function __construct(){
-  	$this->output = $output;
+  	//$this->output = $output;
   }
-  
-  
+  public function setResponse($code, $result = null){
+    switch($code){
+      //add as many as cases you want
+      case 0: $this->responseOutput = array('status_code'=> 0,'status'=>'success','message'=>'Request successfull','response'=> $result);
+      break;
+
+      case 1: $this->responseOutput = array('status_code'=> 1,'status'=>'error','message'=>'request was unsuccessful','response'=> $result);
+      break;
+
+      case 9: $this->responseOutput = array('status_code'=> 9,'status'=>'error','message'=>'Unauthorized Request','response'=> $result);
+      break;
+
+      case 51: $this->responseOutput = array('status_code'=> 51,'status'=>'error','message'=>'Password Doesnot Match','response'=> $result);
+      break;
+
+      case 101: $this->responseOutput = array('status_code'=> 101,'status'=>'success','message'=>'Registration Successful','response'=> $result);
+      break;
+
+      case 401: $this->responseOutput = array('status_code'=> 401,'status'=>'error','message'=>'Unable to Process','response'=> $result);
+      break;
+
+      // Make your own Cases
+        
+      default: $this->responseOutput = array('status_code'=> 666,'status'=>'error','message'=>'unccessfull','response'=> $result);
+      break;
+    }
+  }
+  public function getResponse(){
+    return $this->responseOutput;
+  }
+
+//==============================================================================
   public function set($name, $value){
     $this->output[$name] = $value;
   }
-  
   public function output($format="array"){
     switch($format){
-    
       case "json":
         return json_encode($this->output);
         break;
-        
       case "array":
       	return $this->output;
       	break;
-      	
       default:
         return "invalid_output_format";
     }
   }
-  
+//==============================================================================
 }
-
 ?>
